7d08f40a85e6a6bfbf57b14f6499f79c560a1c16
Michael Klepac <mklepac90@gmail.com>
2020-08-26 18:40:39 +0000

    implemented stdin and handler for terminating game

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const { connect } = require('./client');
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (handleUserInput));
+  return stdin;
+}
+
+handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
 console.log('Connecting ...');
+setupInput();
 connect();
